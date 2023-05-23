6c035daa403d9bf823cd219dd491d12d9052dd2d
songkangle <songkl@novastar.tech>
2023-05-23 16:12:03 +0800

    feat: git命令交互式提交代码

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,17 @@
 // 编辑commitlint.config.js，导入.cz-config.js 中的自定义的规则项
-const { types } = require("./.cz-config.js");
+const { types } = require('./.cz-config.js');
 const typeList = types.map((item) => item.value);
 
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [2, "always", typeList],
+    'type-enum': [2, 'always', typeList],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72],
   },
 };
