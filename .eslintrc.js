9a5e328f1d5fb0650a95f637238a15c5cf99c4d0
songkangle <songkl@novastar.tech>
2023-05-23 17:31:19 +0800

    feat: eslint的引入

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'array-callback-return': 2,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 function foo() {
   console.log('Hello,foo'); // 这里故意不缩进
 }
+
+let list = [1, 2, 3, 4, 5].map((item) => {
+  item = item * 2;
+});
